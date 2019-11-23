9f72e8a84e6ec28447141580b2a412a061af8690
Adam Sobotka <adam.sobotka@socialbakers..com>
2019-11-23 17:30:36 +0100

    routy a tak

---
+++ src/routes/vystoupeni.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Vystoupení/title>
+</svelte:head>
+
+<h1>claim</h1>
+
+<p>paragraph</p>
---
+++ src/routes/workshopy.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Workshopy</title>
+</svelte:head>
+
+<h1>claim</h1>
+
+<p>paragraph</p>
