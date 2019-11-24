0d9a52867c5aafb2d21182a992218bf99d9406e7
Adam Sobotka <adam.sobotka@socialbakers..com>
2019-11-24 15:23:05 +0100

    kontaktystatic

--- src/routes/kontakty.svelte
+++ src/routes/kontakty.svelte
@@ -9,12 +9,9 @@
 <section>
     <h1 aria-label="Nadpis">Kontakty</h1>
     <p aria-label="Popis">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod justo suscipit scelerisque fermentum.
-        Etiam vitae facilisis tellus, nec dictum metus. Etiam viverra enim venenatis, mollis mauris in, maximus magna.
-        Sed nunc enim, consectetur dignissim porta at, dapibus in turpis. Suspendisse ultricies pretium metus lobortis eleifend.
-        Aliquam in finibus dui, non malesuada odio. Maecenas ut leo tristique, posuere est a, scelerisque tortor. Nam volutpat
-        faucibus scelerisque.
+       Máte-li jakékoliv dotazy ke kurzům nebo vystoupením, neváhejte mi napsat na <a href="mailto: ptejse@budecirkus.com">email</a> nebo na <a href="https://www.facebook.com/budecirkus">facebook</a>
     </p>
+    <p aria-label="Telefon"> 777 225 865</p>
     <figure aria-label="Obrázek k workshopu">
         <img src="photo2.jpg" alt="dítě v akrobatické šále s dozorem">
     </figure>
