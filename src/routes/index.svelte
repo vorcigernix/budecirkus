028e368f4fe649f31538ed6309075292889c6676
Adam Sobotka <adam.sobotka@socialbakers..com>
2019-11-24 09:58:39 +0100

    nevim

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -33,14 +33,14 @@
 </style>
 
 <svelte:head>
-	<title>Obohaďte se Novým Cirkusem</title>
+	<title>Obohaťte se Novým Cirkusem</title>
 </svelte:head>
 
 <h1>Great success!</h1>
 
 <figure>
 	<img alt='Borat' src='great-success.png'>
-	<figcaption>Obohaďte se Novým Cirkusem</figcaption>
+	<figcaption>Obohaťte se Novým Cirkusem</figcaption>
 </figure>
 
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
