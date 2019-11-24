861eacee84d2fef12de1cccfd8e206eaee838d5b
Adam Sobotka <adam.sobotka@socialbakers..com>
2019-11-24 10:04:40 +0100

    kurzyback

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -20,9 +20,8 @@
       <a class:selected={segment === 'workshopy'} href="workshopy">workshopy</a>
     </li>
 	<li><a class:selected='{segment === "kontakty"}' href='kontakty'>kontakty</a></li>
-    <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen
+
 		<li><a rel=prefetch class:selected='{segment === "blog"}' href='blog'>blog</a></li>
-		-->
+
   </ul>
 </nav>
--- src/routes/kurzy.svelte
+++
@@ -1,7 +0,0 @@
-<svelte:head>
-	<title>Kurzy</title>
-</svelte:head>
-
-<h1>claim</h1>
-
-<p>paragraph</p>
