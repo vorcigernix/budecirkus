a6a78fcb0b9d75ae12fa573fa54135c07daa8fec
Adam Sobotka <adam.sobotka@socialbakers..com>
2019-11-24 15:42:13 +0100

    meta

--- src/routes/kontakty.svelte
+++ src/routes/kontakty.svelte
@@ -1,40 +1,44 @@
 
 <style>
+    h1 {
+        font-weight: 500;
+        font-size: 2.5rem;
+        color: var(--orange);
+    }
+
+    .card {
+        width: 95%;
+        height: auto;
+        padding: 2rem;
+        margin-bottom: 1rem;
+        background-color: var(--white);
+        border-radius: .5rem;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.08);
+        text-align: left;
+    }
+
+    .image {
+        width: 100%;
+    }
+
+    .figure {
+        margin: 0;
+        padding-top: 2rem;
+    }
 </style>
 
 <svelte:head>
     <title>Kontakty</title>
 </svelte:head>
 
-<section>
+
+<section class="card">
     <h1 aria-label="Nadpis">Kontakty</h1>
-    <p aria-label="Popis">
+    <p class='description' aria-label="Popis">
        Máte-li jakékoliv dotazy ke kurzům nebo vystoupením, neváhejte mi napsat na <a href="mailto: ptejse@budecirkus.com">email</a> nebo na <a href="https://www.facebook.com/budecirkus">facebook</a>
     </p>
     <p aria-label="Telefon"> 777 225 865</p>
-    <figure aria-label="Obrázek k workshopu">
-        <img src="photo2.jpg" alt="dítě v akrobatické šále s dozorem">
+    <figure class='figure' aria-label="Obrázek ke kontaktu">
+        <img class='image' src="photo2.jpg" alt="dítě v akrobatické šále s dozorem">
     </figure>
-</section>
-
-<section>
-    <h1 aria-label="Nadpis">Akrobatická vystoupení</h1>
-    <p aria-label="Popis">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod justo suscipit scelerisque fermentum.
-        Etiam vitae facilisis tellus, nec dictum metus. Etiam viverra enim venenatis, mollis mauris in, maximus magna.
-        Sed nunc enim, consectetur dignissim porta at, dapibus in turpis. Suspendisse ultricies pretium metus lobortis eleifend.
-        Aliquam in finibus dui, non malesuada odio. Maecenas ut leo tristique, posuere est a, scelerisque tortor. Nam volutpat
-        faucibus scelerisque.
-    </p>
-    <figure aria-label="Obrázek k akrobatickému vystoupení">
-        <img src="photo1.jpg" alt="mladá slečna zavěšená v cirkusovém kruhu před publikem">
-    </figure>
-</section>
-
-<section>
-    <h1 aria-label="Odkaz na kontakty">
-        <a href="kontakty">
-            Kontaktujte nás
-        </a>
-    </h1>
 </section>
