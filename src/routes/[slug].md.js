6dfa2c25ccdd8d8028221d02baa5a3bc2e378d4c
Adam Sobotka <adam.sobotka@socialbakers..com>
2019-11-24 12:05:28 +0100

    progress in md

---
+++ src/routes/[slug].md.js
@@ -0,0 +1,50 @@
+import fs from 'fs';
+import { extract_frontmatter, link_renderer } from '@sveltejs/site-kit/utils/markdown.js';
+import marked from 'marked';
+import { makeSlugProcessor } from '../helpers/slug';
+
+export const SLUG_PRESERVE_UNICODE = false;
+export const SLUG_SEPARATOR = '_';
+const makeSlug = makeSlugProcessor(SLUG_PRESERVE_UNICODE);
+
+export function get(req, res) {
+    const file = `content/${params.slug}.md`;
+    const match = /^(\d+-\d+-\d+)-(.+)\.md$/.exec(file);
+    if (!match) throw new Error(`Invalid filename '${file}'`);
+
+    const [, pubdate, slug] = match;
+
+    const markdown = fs.readFileSync(`content/blog/${file}`, 'utf-8');
+
+    const { content, metadata } = extract_frontmatter(markdown);
+
+    const date = new Date(`${pubdate} EDT`); // cheeky hack
+    metadata.pubdate = pubdate;
+    metadata.dateString = date.toDateString();
+
+    const renderer = new marked.Renderer();
+
+    renderer.link = link_renderer;
+
+    renderer.heading = (text, level, rawtext) => {
+        const fragment = makeSlug(rawtext);
+
+        return `
+					<h${level}>
+						<span id="${fragment}" class="offset-anchor"></span>
+						<a href="blog/${slug}#${fragment}" class="anchor" aria-hidden="true"></a>
+						${text}
+					</h${level}>`;
+    };
+
+    const html = marked(
+        content.replace(/^\t+/gm, match => match.split('\t').join('  ')),
+        { renderer }
+    );
+
+    return {
+        html,
+        metadata,
+        slug
+    };
+}
---
+++ src/routes/kontakty/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+	export async function preload({ params }) {
+		const res = await this.fetch(`../${params.slug}.md`);
+		return res.ok ? { post: await res.json() } : this.error(404, 'Not found');
+	}
+</script>
+
+<script>
+	export let post;
+</script>
+
+<svelte:head>
+	<title>Kontakty</title>
+</svelte:head>
+
+<h1>claim</h1>
+
+<p>paragraph</p>
