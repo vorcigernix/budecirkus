10c288a49ea485be87bb8ac0d272c968d055a971
Jan Kraus <jan.kraus@socialbakers.com>
2019-11-24 15:23:19 +0100

    vsechno

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,12 @@
+<script>
+    const goTo = (route) => {
+        window.location.href = route;
+    }
+</script>
 <style>
 :global(body) {
     background-color: var(--lightgrey);
-    height: 200vh;  
+    height: 200vh;
 }
 
 h1 {
@@ -75,21 +80,90 @@ h2 {
     color: var(--orange);
 }
 
+.storyP {
+    padding: 1rem;
+    text-align: justify;
+}
+
+.photo {
+    width: 100%;
+}
+
+.name {
+    font-size: 1.5rem;
+    color: var(--blue);
+    text-align: center;
+}
+
 </style>
 
 <svelte:head>
 	<title>Bude Cirkus</title>
 </svelte:head>
 
-<section class="landing">
-    <h1>Obohaťte se <strong>novým cirkusem</strong></h1>
-    <h2>Výjimečný. Jedinečný. Dechberoucí.</h2>
-    <div class="box">
+<section class="landing" aria-label="Hlavní stránka, první část">
+    <h1 aria-label="Heslo Bude Cirkus">Obohaťte se <strong>novým cirkusem</strong></h1>
+    <h2 aria-label="Podnadpis s popiskama">Výjimečný. Jedinečný. Dechberoucí.</h2>
+    <div class="box" on:click={() => goTo('/kurzy')} aria-label="Kurzy">
         <p>Pořádáme kurzy pro děti od 3 do 15 let a workshopy pro malé i velké.</p>
         <a href="kurzy">Přihlaste se do kurzu</a>
     </div>
-    <div class="box">
+    <div class="box" on:click={() => goTo('/nabizime')} aria-label="Nabídka">
         <p>Nabízíme netradiční vystoupení na plesech, firemních akcích i festivalech</p>
-        <a href="vystoupeni">Objednejte si nás</a>
+        <a href="vystoupeni" aria-label="Odkaz na nabídku">Objednejte si nás</a>
+    </div>
+</section>
+
+<section class="landing" aria-label="Náš příběh">
+    <h1 aria-label="Nadpis příběhu">Můj příběh</h1>
+    <h2 aria-label="Úvod příběhu">Toto je můj příběh o tom, jak se koníček stal prací.</h2>
+    <div class="box">
+        <p class="storyP" aria-label="První odstavec příběhu">
+            Když jsem byla malá, začala jsem dělat atletiku. Po ukončení střední školy jsem nastoupila na studium
+            v Praze, kde jsem studovala chemii životního prostředí a zároveň jsem se věnovala atletice. Dostala jsem
+            příležitost vyjet na Erasmus do Granady (Španělsko) na dva semestry. Už jsem odjížděla s myšlenkou najít a
+            zapojit se do nového cirkusu, kdyby tam byl.
+        </p>
+        <p class="storyP" aria-label="Druhý odstavec příběhu">
+            Odjela jsem na Erasmus a začala jsem chodit na cirkus. Je to zábavné, nesoutěžní a pro všechny. Líbila se
+            mi přátelská atmosféra na kurzech a když jsem se vrátila do Čech, věděla jsem, že se tomu chci věnovat.
+        </p>
+        <p class="storyP" aria-label="Třetí odstavec příběhu">
+            Začala jsem učit děti na kurzu a paralelně k tomu trénovala atletiku a chodila na vysokou školu. Po studiu
+            jsem se domluvila s přítelem, že se přestěhuji za ním do Českých Budějovic.
+        </p>
+        <p class="storyP" aria-label="Čtvrtý odstavec příběhu">
+            Nebyl tam žádný cirkus, tak jsem sama otevřela jeden kurz, na začátku spíše pro kamarády, a také jsem začala
+            vystupovat. Dneska máme 9 kurzů pro dětí a 2 pro dospěle. Můj koníček se stal mojí prací. Do budoucna chceme
+            připravovat více workshopů, kulturních akcí a festivalů.
+        </p>
+    </div>
+</section>
+
+<section class="landing" aria-label="Lektoři">
+    <h1 aria-label="Nadpis sekce">Naši lektoři</h1>
+    <div class="box">
+        <p class="name" aria-label="Jméno lektorky">Katka Vaněčková</p>
+        <figure class='figure' >
+            <img class='photo' src="lektor1.png" alt="Lektorka Katka Vaněčková">
+        </figure>
+    </div>
+    <div class="box">
+        <p class="name" aria-label="Jméno lektorky">Katka Vaněčková</p>
+        <figure class='figure' >
+            <img class='photo' src="lektor2.png" alt="Lektorka Katka Vaněčková">
+        </figure>
+    </div>
+    <div class="box">
+        <p class="name" aria-label="Jméno lektorky">Katka Vaněčková</p>
+        <figure class='figure' >
+            <img class='photo' src="lektor3.png" alt="Lektorka Katka Vaněčková">
+        </figure>
+    </div>
+    <div class="box">
+        <p class="name" aria-label="Jméno lektorky">Katka Vaněčková</p>
+        <figure class='figure' >
+            <img class='photo' src="lektor4.png" alt="Lektorka Katka Vaněčková">
+        </figure>
     </div>
 </section>
--- src/routes/nabizime/index.svelte
+++ src/routes/nabizime/index.svelte
@@ -1,43 +1,78 @@
+<script>
+    const goTo = (route) => {
+        window.location.href = route;
+    }
+</script>
 
 <style>
+    h1 {
+        font-weight: 500;
+        font-size: 2.5rem;
+        color: var(--orange);
+    }
+
+    .card {
+        width: 95%;
+        height: auto;
+        padding: 2rem;
+        margin-bottom: 1rem;
+        background-color: var(--white);
+        border-radius: .5rem;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.08);
+        text-align: left;
+    }
+
+    .image {
+        width: 100%;
+    }
+
+    .figure {
+        margin: 0;
+        padding-top: 2rem;
+    }
+
+    .contact {
+        padding: 1rem;
+        margin-bottom: 1rem;
+        text-align: center;
+    }
+
 </style>
 
 <svelte:head>
     <title>Nabízíme</title>
 </svelte:head>
 
-<section>
+<section class="card">
     <h1 aria-label="Nadpis">Workshopy</h1>
-    <p aria-label="Popis">
+    <p class='description' aria-label="Popis">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod justo suscipit scelerisque fermentum.
         Etiam vitae facilisis tellus, nec dictum metus. Etiam viverra enim venenatis, mollis mauris in, maximus magna.
         Sed nunc enim, consectetur dignissim porta at, dapibus in turpis. Suspendisse ultricies pretium metus lobortis eleifend.
         Aliquam in finibus dui, non malesuada odio. Maecenas ut leo tristique, posuere est a, scelerisque tortor. Nam volutpat
         faucibus scelerisque.
     </p>
-    <figure aria-label="Obrázek k workshopu">
-        <img src="photo2.jpg" alt="dítě v akrobatické šále s dozorem">
+    <figure class='figure' aria-label="Obrázek k workshopu">
+        <img class='image' src="photo2.jpg" alt="dítě v akrobatické šále s dozorem">
     </figure>
 </section>
 
-<section>
+<section class="card">
     <h1 aria-label="Nadpis">Akrobatická vystoupení</h1>
-    <p aria-label="Popis">
+    <p class='description' aria-label="Popis">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod justo suscipit scelerisque fermentum.
         Etiam vitae facilisis tellus, nec dictum metus. Etiam viverra enim venenatis, mollis mauris in, maximus magna.
         Sed nunc enim, consectetur dignissim porta at, dapibus in turpis. Suspendisse ultricies pretium metus lobortis eleifend.
         Aliquam in finibus dui, non malesuada odio. Maecenas ut leo tristique, posuere est a, scelerisque tortor. Nam volutpat
         faucibus scelerisque.
     </p>
-    <figure aria-label="Obrázek k akrobatickému vystoupení">
-        <img src="photo1.jpg" alt="mladá slečna zavěšená v cirkusovém kruhu před publikem">
+    <figure class='figure' aria-label="Obrázek k akrobatickému vystoupení">
+        <img  class='image' src="photo1.jpg" alt="mladá slečna zavěšená v cirkusovém kruhu před publikem">
     </figure>
 </section>
 
-<section>
-    <h1 aria-label="Odkaz na kontakty">
-        <a href="kontakty">
-            Kontaktujte nás
-        </a>
+<section class='card contact'>
+    <h1 aria-label="Odkaz na kontakty" on:click={() => goTo('/kontakty')}>
+        Kontaktujte nás
     </h1>
 </section>
