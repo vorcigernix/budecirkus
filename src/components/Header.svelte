4fd3cca7b5361da185fda3d8e6bc2e987745e110
Jan Kraus <jan.kraus@socialbakers.com>
2019-11-24 09:59:46 +0100

    cleanup

---
+++ src/components/Header.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Nav from '../components/Nav.svelte';
+    export let segment;
+</script>
+
+<style>
+</style>
+
+<div>
+    <a href="." >
+        <img alt="Logo" src="/assets/logo.png" />
+    </a>
+    <div>
+        <Nav {segment} />
+    </div>
+</div>
